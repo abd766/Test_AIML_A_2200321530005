77c6649f34a6465cb64bbb19f175b20dcd781c00
Abd766 <abdullahmasood766@gmail.com>
2024-11-07 15:13:56 +0530

    Final changes

--- JS/login.js
+++ JS/login.js
@@ -1,10 +1,10 @@
 function validate(){
-    let userid = document.getElementById("userid")
-    let password = document.getElementById("pwd")
+    let userid = document.getElementById("userid").value;
+    let password = document.getElementById("pwd").value;
     if(userid != "admin" || password != "admin"){
         alert("Please enter valid credentials")
     }
     else{
-        
+
     }
 }
