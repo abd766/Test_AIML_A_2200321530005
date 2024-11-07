cf602d04a6f154051bda45ce5548f681878f2a37
Abd766 <abdullahmasood766@gmail.com>
2024-11-07 15:00:44 +0530

    Basic structural files added and skeleton made

---
+++ JS/login.js
@@ -0,0 +1,10 @@
+function validate(){
+    let userid = document.getElementById("userid")
+    let password = document.getElementById("pwd")
+    if(userid != "admin" || password != "admin"){
+        alert("Please enter valid credentials")
+    }
+    else{
+        
+    }
+}
